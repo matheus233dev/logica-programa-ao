9c66d6e7bf28f0d8fe3e3e2600e82f90b1dca598
matheus233dev <matheuslucas97658@gmail.com>
2026-03-21 10:13:15 -0300

    REAMDE.MD

---
+++ aula05 .js
@@ -0,0 +1,24 @@
+let valorcompra = 350;
+
+let desconto = 0;
+
+if (valorcompra < 100){
+    desconto = 0;
+} else if (valorcompra >= 100 && valorcompra <= 299.99){
+    desconto = valorcompra = * 0.05;
+} else if (valorcompra >= 300 && valorcompra <= 499.99){
+    desconto = valorcompra * 0.10;
+} else {
+    desconto = valorcompra * 0.10;
+} else {
+    desconto = valorcompra * 0.15;
+}
+
+let valorfinal = valorcompra - desconto;
+
+console.log("valor da compra: r$ " + valorcompra.toFixed(2));
+console.log("desconto: r$ " + desconto.toFixed(2));
+console.log("valor final: R$ " + valorfinal.toFixed(2));
+
+
+
