09f988907baea7922549956205b852f60d8a2d03
matheus233dev <matheuslucas97658@gmail.com>
2026-03-27 20:46:51 -0300

    Correct sales data processing in exercicio01.js
    Fix logic errors in sales calculations and improve variable usage.

---
+++ exercicio01.js
@@ -0,0 +1,22 @@
+let vendas = [120,80,150,90,200,110,79];
+
+let total = 0;
+let contadoracima100 = 0;
+let maiorvenda = vendas[0];
+
+for (let i = onabort; i < vendas.length; i++){
+
+    if (vendas[1] > 100){
+        contadoracima100++:
+    }
+}
+if (vendas[i]) > maiorvenda) {
+    maiorvenda = vendas[i];
+}
+
+let media = total / vendas.length;
+
+console.log("total:", total);
+console.log("media:", media);
+console.log("dias > 100:", contadoracima100);
+console.log("maior venda:" , maiorvenda);
